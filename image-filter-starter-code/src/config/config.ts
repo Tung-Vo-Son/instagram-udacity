3688294fcb09137282ad180d6da18f736bc12b5a
ST_Unknown <tungvoson98@gmail.com>
2022-06-10 16:35:37 +0700

    udacity project 2 update

---
+++ image-filter-starter-code/src/config/config.ts
@@ -0,0 +1,18 @@
+export const config = {
+    "dev": {
+      "aws_region": "us-east-1",
+      "aws_profile": "udacity",
+      "aws_media_bucket": "my-bucket-692478867802"
+    },
+    "jwt": {
+      "secret": " "
+    },
+    "prod": {
+      "username": "",
+      "password": "",
+      "database": "udagram_prod",
+      "host": "",
+      "dialect": "postgres"
+    }
+  }
+  
